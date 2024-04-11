710e81d01c1ffcd6486a9b7ff3896f80e065ebdf
Erkut <67333330+Erkutkenar@users.noreply.github.com>
2024-04-11 18:38:52 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import api from './api/axionsConfig';
+import { useState, useEffect } from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
